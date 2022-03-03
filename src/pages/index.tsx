da9aa55ee7eed84eb4f622bb30b2b2824bcd3831
Jeferson Mesquita <je_2742@hotmail.com>
2022-03-03 00:33:51 -0300

    Componentes e paginas done

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,31 @@
 import Head from 'next/head';
 
+import { SubscribeButton } from '../components/SubscribeButton';
+
+import styles from './home.module.scss';
+
 export default function Home() {
   return (
-    <h1>
+    <>
       <Head>
-        <title>Início | ig.news</title>
+        <title>Home | ig.news</title>
       </Head>
-      Hello World
-    </h1>
+
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👋 Hey, welcome</span>
+          <h1>
+            News about the <span>React</span> world.
+          </h1>
+          <p>
+            Get acess all the publications <br />
+            <span>for 9,90 $month</span>
+          </p>
+          <SubscribeButton />
+        </section>
+
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
+    </>
   );
 }
