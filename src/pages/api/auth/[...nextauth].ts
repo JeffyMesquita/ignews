e7025b961f6f1115988f46dd9297281949694529
Jeferson Mesquita <je_2742@hotmail.com>
2022-05-21 09:44:04 -0300

    Aula  2-3

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,20 +1,43 @@
 import NextAuth from 'next-auth';
 import GithubProvider from 'next-auth/providers/github';
+import { query as q } from 'faunadb';
 
-// import { query as q } from 'faunadb';
-
-// import { fauna } from '../../../services/fauna';
+import { fauna } from '../../../services/fauna';
 
 export default NextAuth({
   providers: [
     GithubProvider({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
       authorization: {
         params: {
           scope: 'read:user',
         },
-      }
+      },
     }),
   ],
+  secret: process.env.NEXTAUTH_SECRET,
+  callbacks: {
+    async signIn({ user }) {
+      const { email } = user;
+
+      await fauna.query(
+        q.Create(q.Collection('users'), {
+          data: {
+            email,
+          },
+        })
+      );
+
+      return true;
+    },
+  },
 });
+
+// FaunaDB - HTTP
+// DynamoDB -AWS
+
+// PostgreSQL, MongoDB,
+
+// 24h (1 conexão por dia)
+// 1000 autenticações ()
